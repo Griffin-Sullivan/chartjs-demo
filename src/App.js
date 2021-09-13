0d2cda12446f5f0efa1f0a3461bf05e47736367a
Griffin Sullivan <griffinsully@Griffins-MacBook-Air.local>
2021-09-13 15:39:14 -0400

    adding doughnut

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Doughnut } from 'react-chartjs-2';
+
+const data = {
+  labels: ['Food', 'Rent', 'Entertainment', 'Utilities', 'Loans', 'Gas'],
+  datasets: [
+    {
+      label: 'Monthly Cost Breakdown',
+      data: [223, 1450, 45, 120, 600, 115],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+  width: 400,
+  height: 400,
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Monthly Cost Breakdown
+      </h1>
+      <div className="App">
+        <Doughnut 
+          data={data}
+          width={400}
+          height={400}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
     </div>
+    
   );
 }
 
