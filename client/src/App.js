8ba3f85fa3592b0c7bec80c9901e430630acb340
Griffin Sullivan <gxs331@psu.edu>
2021-09-24 14:29:15 -0400

    adding node

---
+++ client/src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+import { Doughnut } from 'react-chartjs-2';
+import React, { useState, useRef } from 'react';
+
+function App() {
+  const [food, setFood] = useState(0);
+  const doughnut = useRef();
+
+  const updateChart = async e => {
+    e.preventDefault();
+    doughnut.current.data.datasets[0].data = [food, 1450, 45, 120, 600, 115]
+    doughnut.current.update()
+    
+    const response = await fetch('/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ food: food }),
+    });
+    const body = await response.text();
+  }
+
+  const data = {
+    labels: ['Food', 'Rent', 'Entertainment', 'Utilities', 'Loans', 'Gas'],
+    datasets: [
+      {
+        label: 'Monthly Cost Breakdown',
+        data: [0, 1450, 45, 120, 600, 115],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+
+  return (
+    <div className="App">
+      <h1>
+        Monthly Cost Breakdown
+      </h1>
+      <div className="App">
+        <Doughnut 
+          data={data}
+          ref={doughnut}
+          width={400}
+          height={400}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
+      <div className="App">
+        <button onClick={updateChart}>Submit</button>
+        <label>
+          Food: 
+          <input value={food} onInput={(food) => setFood(food.target.value)} />
+        </label>
+      </div>
+      
+    </div>
+    
+  );
+}
+
+export default App;
